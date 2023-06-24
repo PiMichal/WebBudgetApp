966d27b6deac5c0f5b0e1fcb5e599f1cd74525b1
PiMichal <michal.pieroog@gmail.com>
2023-06-24 15:02:49 +0200

    Added sessions

--- App/Controllers/Login.php
+++ App/Controllers/Login.php
@@ -34,16 +34,45 @@ class Login extends \Core\Controller
 
         if ($user) {
 
-            $this->redirect('/home/index');
+            session_regenerate_id(true);
 
+            $_SESSION['user_id'] = $user->id;
+
+            $this->redirect('/home/index');
         } else {
             View::renderTemplate('Login/new.html', [
                 'email' => $_POST['email'],
             ]);
         }
-
     }
+    /**
+     * Log out a user
+     * 
+     * @return void
+     */
+    public function destroyAction()
+    {
+        // Unset all of the session variables.
+        $_SESSION = [];
 
+        // Delete the session cookie
+        if (ini_get("session.use_cookies")) {
+            $params = session_get_cookie_params();
 
+            setcookie(
+                session_name(),
+                '',
+                time() - 42000,
+                $params["path"],
+                $params["domain"],
+                $params["secure"],
+                $params["httponly"]
+            );
+        }
+
+        // Finally, destroy the session.
+        session_destroy();
+
+        $this->redirect('/login/new');
+    }
 }
-
